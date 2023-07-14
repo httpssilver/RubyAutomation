42f2c9ac18be9e3119f6a5bd50504b9fc8328ca0
hardishe <139243392+hardishe@users.noreply.github.com>
2023-07-14 16:35:20 +0300

    Update RubyAutomation.py

--- RubyAutomation.py
+++ RubyAutomation.py
@@ -37,7 +37,7 @@ except ModuleNotFoundError:
         ask = input("Installation finished.")
         exit()
 
-scriptVersion = 6
+scriptVersion = 5
 def whichPythonCommand():
     LocalMachineOS = platform.system()
     if (
